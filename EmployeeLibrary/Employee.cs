4186f8671623ce6d83ff3777ca513591d1468dab
XemeChen <xemechen@hotmail.com>
2015-10-30 20:34:14 +0800

    SalaryCheckAssignment20151030

---
+++ EmployeeLibrary/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLibrary
+{
+    public class Employee
+    {
+        private int salary;
+        private int basesalary;
+        private int benefit;
+
+        public int BaseSalary
+        {
+            get { return basesalary; }
+            set { if (value < 0) value = 0; else if (value > 100000) value = 100000; basesalary = value; }
+        }
+
+
+        public int Benefit
+        {
+            get { return benefit; }
+            set { if (value < 0) value = 0; else if (value > 50000) value = 50000; benefit = value; }
+        }
+
+        public int Salary
+        {
+            get { return basesalary + benefit; }
+            set { salary = value; }
+        }
+
+
+
+        private string firstName;
+        private string lastName;
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+
+        public Employee(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+
+    }
+}
---
+++ SalaryCheck/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeLibrary;
+
+namespace SalaryCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeLibrary.Employee employee = new EmployeeLibrary.Employee("Andrew", "Xeme");
+            //Employee Andrew=new Employee("Andrew","Xeme");
+
+            //Console.WriteLine("Input your First Name");
+            //employee.FirstName = Console.ReadLine();
+            //Console.WriteLine("Input your Last Name");
+            //employee.LastName = Console.ReadLine();
+
+            Console.WriteLine("Input your base salary (0-100,000)");
+            string basesalary=Console.ReadLine();
+            
+            int bsalary; int benefi;
+            Console.WriteLine("Input your benefit (0-50,000)");
+            string benefit = Console.ReadLine();
+            int.TryParse(basesalary, out bsalary);
+            int.TryParse(benefit, out benefi);
+            employee.Benefit=benefi;
+            employee.BaseSalary=bsalary;
+            Console.WriteLine(employee.FirstName+" "+employee.LastName+
+                ", your total income is " + employee.Salary+".");
+
+        }
+    }
+}
